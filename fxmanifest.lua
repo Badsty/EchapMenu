d88f8353b097c0051ec075941cf6d92ff0a0d80e
SLW badsty <111683903+Badsty@users.noreply.github.com>
2023-04-26 15:07:03 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'slw'
+
+
+--Client Scripts-- 
+client_scripts {
+ 'Client/*.lua'
+}
+
+--Server Scripts-- 
+server_scripts {
+ 'Server/*.lua'
+}
+
+
+--UI Part-- 
+ui_page {
+ 'html/index.html', 
+}
+
+--File Part-- 
+files {
+ 'html/index.html',
+ 'html/app.js', 
+ 'html/style.css'
+} 
